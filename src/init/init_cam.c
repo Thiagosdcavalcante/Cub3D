3e5c84b3415b0bdc2d4563546792e2470e799fad
THIAGO SANTANA DANTAS CAVALCANTE <61096544+Thiagosdcavalcante@users.noreply.github.com>
2025-02-13 12:15:20 -0300

    CHANGED: function start_cam_infor isnt static now

--- src/init/init_cam.c
+++ src/init/init_cam.c
@@ -6,13 +6,13 @@
 /*   By: tsantana <tsantana@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 11:54:52 by tsantana          #+#    #+#             */
-/*   Updated: 2025/02/13 12:08:45 by tsantana         ###   ########.fr       */
+/*   Updated: 2025/02/13 12:13:16 by tsantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-static void	start_cam_infor(t_game *game)
+void	start_cam_infor(t_game *game)
 {
 	game->cam->plr_x = game->player.dir_x * game->tile.base + game->tile.base / 2;
 	game->cam->plr_y = game->player.dir_y * game->tile.base + game->tile.base / 2;
