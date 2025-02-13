6c09957f232d15e249dd5d7539a5a78b85fd73ab
THIAGO SANTANA DANTAS CAVALCANTE <61096544+Thiagosdcavalcante@users.noreply.github.com>
2025-02-12 23:51:52 -0300

    fix: .xpm to .png

--- src/parsing/parse_args.c
+++ src/parsing/parse_args.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_args.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leobarbo <leobarbo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: tsantana <tsantana@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 19:30:05 by leobarbo          #+#    #+#             */
-/*   Updated: 2025/01/20 03:31:58 by leobarbo         ###   ########.fr       */
+/*   Updated: 2025/02/12 23:38:56 by tsantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static bool	parse_xpm_extension(char *file)
 	len = ft_strlen(file);
 	if (len < 4)
 		return (false);
-	return (!special_ft_strncmp(file + len - 4, ".xpm", 4)); // resolver isso, tive que colocar por conta do '\r'
+	return (!special_ft_strncmp(file + len - 4, ".png", 4)); // resolver isso, tive que colocar por conta do '\r'
 }
 
 static bool	is_dir(char *arg)
@@ -61,6 +61,6 @@ int	parse_file(char *arg, bool cub)
 			return (err_msg(Y "Not a .cub file" RST, 1));
 	}
 	if (!cub && !parse_xpm_extension(arg))
-		return (err_msg(Y "Not a .xpm file" RST, 1)); // mudar para png.
+		return (err_msg(Y "Not a .png file" RST, 1)); // mudar para png.
 	return (0);
 }
