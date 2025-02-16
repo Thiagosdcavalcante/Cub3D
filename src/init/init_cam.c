8bb32d1e58ded309515cd5076a09f0fe230ebe60
tsantana <tsantana@student.42sp.org.br>
2025-02-16 20:21:21 -0300

    FIX: start_cam_infor

--- src/init/init_cam.c
+++ src/init/init_cam.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_cam.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsantana <tsantana@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: tsantana <tsantana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 11:54:52 by tsantana          #+#    #+#             */
-/*   Updated: 2025/02/13 13:05:38 by tsantana         ###   ########.fr       */
+/*   Updated: 2025/02/16 20:08:16 by tsantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	start_cam_infor(t_game *game)
 {
-	game->cam->plr_x = game->player.pos_x * game->tile.base + game->tile.base / 2;
-	game->cam->plr_y = game->player.pos_y * game->tile.base + game->tile.base / 2;
+	game->cam->plr_x = (game->player.pos_x * game->tile.base) + game->tile.base / 2;
+	game->cam->plr_y = (game->player.pos_y * game->tile.base) + game->tile.base / 2;
 	game->cam->fov_plr = (FOV * M_PI) / 180;
 	if (game->player.dir == 'N')
 		game->cam->angle = M_PI * NORTH;
@@ -23,5 +23,7 @@ void	start_cam_infor(t_game *game)
 		game->cam->angle = M_PI * SOUTH;
 	else if (game->player.dir == 'W')
 		game->cam->angle = M_PI * WEST;
+	else
+		game->cam->angle = M_PI * EAST;
 	printf("plr_x: %f plr_y: %f dir: %c angle: %f\n", game->player.pos_x, game->player.pos_y, game->player.dir, game->cam->angle);
 }
