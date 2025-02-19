42ae9a1ffc85be30947dca83b4bcfcb472f4071f
THIAGO SANTANA DANTAS CAVALCANTE <61096544+Thiagosdcavalcante@users.noreply.github.com>
2025-02-19 15:27:35 -0300

    fix: a lot of issue with file ray.c

--- src/render/ray.c
+++ src/render/ray.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ray.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsantana <tsantana@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tsantana <tsantana@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 16:17:12 by tsantana          #+#    #+#             */
-/*   Updated: 2025/02/16 16:16:30 by tsantana         ###   ########.fr       */
+/*   Updated: 2025/02/19 15:26:16 by tsantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,9 +59,9 @@ int	wall_hit(float x, float y, t_game *gm)	// check the wall hit
 	if (x < 0 || y < 0)
 		return (0);
 	map = gm->map_position;
-	x_m = floorf (x / gm->tile.base); // get the x position in the map
-	y_m = floorf (y / gm->tile.base); // get the y position in the map
-	if (y_m > gm->tile.height || x_m > gm->tile.width )
+	x_m = floor(x / gm->tile.base); // get the x position in the map
+	y_m = floor(y / gm->tile.base); // get the y position in the map
+	if (y_m > gm->tile.height || x_m > gm->tile.width)
 		return (0);
 	while (map && map->down && map->line != y_m)
 		map = map->down;
@@ -82,14 +82,12 @@ float	get_h_inter(t_game *gm, float angl)
 
 	y_step = gm->tile.base;
 	x_step = gm->tile.base / tan(angl);
-	//printf("%f\n", angl);
 	h_y = floorf(gm->cam->plr_y / gm->tile.base) * gm->tile.base;
 	pixel = inter_check(angl, &h_y, &y_step, gm);
 	h_x = gm->cam->plr_x + (h_y - gm->cam->plr_y) / tan(angl);
 	if ((unit_circle(angl, 'y') && x_step > 0) || (!unit_circle(angl, 'y') && x_step < 0))
 		x_step *= -1;
-	//printf(" get_h-> %f | %f | %f | %f | %d\n", h_x, h_y, x_step, y_step, pixel);
-	while (wall_hit(h_x, h_y - pixel, gm)) // check the wall hit whit the pixel value
+	while (wall_hit(h_x, h_y - pixel, gm) == 1) // check the wall hit whit the pixel value
 	{
 		h_x += x_step;
 		h_y += y_step;
@@ -107,16 +105,19 @@ float	get_v_inter(t_game *gm, float angl)	// get the vertical intersection
 
 	x_step = gm->tile.base; 
 	y_step = gm->tile.base * tan(angl);
+	gm->is_horizon = !unit_circle(angl, 'x');
 	v_x = floorf(gm->cam->plr_x / gm->tile.base) * gm->tile.base;
 	pixel = inter_check(angl, &v_x, &x_step, gm); // check the intersection and get the pixel value
-	v_y = (gm->cam->plr_y + (v_x - gm->cam->plr_x)) * tan(angl);
-	if ((unit_circle(angl, 'x') && y_step < 0) || (!unit_circle(angl, 'x') && y_step > 0)) // check y_step value
-		y_step *= -1;
-	while (wall_hit(v_x - pixel, v_y, gm)) // check the wall hit whit the pixel value
+	v_y = gm->cam->plr_y + (v_x - gm->cam->plr_x) * tan(angl);
+	if ((y_step < 0) == unit_circle(angl, 'y'))
+		y_step = -y_step;
+	while (wall_hit(v_x - pixel, v_y, gm) == 1) // check the wall hit whit the pixel value
 	{
 		v_x += x_step;
-		v_y += y_step;
+		v_y += y_step;unit_circle
 	}
+	gm->ray.info->x = 0;
+	gm->ray.info->y = v_y;
 	return (sqrt(pow(v_x - gm->cam->plr_x, 2) + pow(v_y - gm->cam->plr_y, 2))); // get the distance
 }
 
@@ -127,22 +128,20 @@ void	cast_rays(t_game *gm)
 	int		ray;
 
 	ray = 0;
-	gm->ray.ray_ngl = gm->cam->angle - ((gm->cam->fov_plr + gm->tile.base) / 2);
-	printf("cast_rays -> %f | %f | %f\n", gm->ray.ray_ngl, gm->cam->angle, gm->cam->fov_plr);
+	gm->ray.ray_ngl = gm->cam->angle - (gm->cam->fov_plr / 2);
 	while (ray < WIDTH)
 	{
 		gm->ray.flag = 0;
 		h_inter = get_h_inter(gm, nor_angle(gm->ray.ray_ngl));
 		v_inter = get_v_inter(gm, nor_angle(gm->ray.ray_ngl));
 		if (v_inter < h_inter)
-			gm->ray.distance = v_inter;
+			gm->ray.distance = get_v_inter(gm, nor_angle(gm->ray.ray_ngl));
 		else
 		{
-			gm->ray.distance = h_inter;
+			gm->ray.distance = get_h_inter(gm, nor_angle(gm->ray.ray_ngl));
 			gm->ray.flag = 1;
 		}
-		render_wall(gm, ray);
-		ray++;
+		render_wall(gm, ray++);
 		gm->ray.ray_ngl += ((double) gm->cam->fov_plr / WIDTH);
 	}
 }
